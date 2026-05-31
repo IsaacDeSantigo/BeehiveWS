a133b71b98b01d51932c718283b28068ec69b39e
IsaacDeSantiago <isaac.desantiago0604@alumnos.udg.mx>
2026-05-31 14:49:47 -0600

    SDMMC (4 Bit) + SAI + 2 Microphones. Not working yet.

--- DataAcquisition/Core/Src/main.c
+++ DataAcquisition/Core/Src/main.c
@@ -138,10 +138,18 @@ int main(void)
 	if (res == FR_OK) {
 		// 2. Crear/Abrir un archivo para escribir (FA_CREATE_ALWAYS pisa el anterior si existe)
 		res = f_open(&fil, "audio.raw", FA_CREATE_ALWAYS | FA_WRITE);
+
+
+		if (HAL_SD_ConfigWideBusOperation(&hsd1, SDMMC_BUS_WIDE_4B) != HAL_OK)
+		    {
+			Error_Handler();
+		    }
 	} else {
 		// Si res no es FR_OK, algo falló en el montaje
 		Error_Handler();
 	}
+
+
 
 	HAL_SAI_Receive_DMA(&hsai_BlockA1,(uint8_t *)audio_buffer, BUFFER_SIZE);
 
@@ -344,14 +352,23 @@ static void MX_SDMMC1_SD_Init(void)
   hsd1.Instance = SDMMC1;
   hsd1.Init.ClockEdge = SDMMC_CLOCK_EDGE_RISING;
   hsd1.Init.ClockPowerSave = SDMMC_CLOCK_POWER_SAVE_DISABLE;
-  hsd1.Init.BusWide = SDMMC_BUS_WIDE_1B;
+  hsd1.Init.BusWide = SDMMC_BUS_WIDE_4B;
   hsd1.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_ENABLE;
-  hsd1.Init.ClockDiv = 0;
+  hsd1.Init.ClockDiv = 2;
   /* USER CODE BEGIN SDMMC1_Init 2 */
-  //if (HAL_SD_ConfigWideBusOperation(&hsd1, SDMMC_BUS_WIDE_4B) != HAL_OK) {
-  //      Error_Handler(); // Si los cables D1, D2 o D3 están mal, caerá aquí.
-  // }
-
+//  // 2. Inicializamos la tarjeta de forma segura en 1-Bit
+//    if (HAL_SD_Init(&hsd1) != HAL_OK)
+//    {
+//        Error_Handler();
+//    }
+//
+//    /* USER CODE BEGIN SDMMC1_Init 2 */
+//    // 3. Una vez inicializada con éxito, AHORA SÍ le decimos a la tarjeta
+//    // y al STM32 que se muevan juntos a 4-Bits mediante la función nativa:
+//    if (HAL_SD_ConfigWideBusOperation(&hsd1, SDMMC_BUS_WIDE_4B) != HAL_OK)
+//    {
+//        Error_Handler();
+//    }
   /* USER CODE END SDMMC1_Init 2 */
 
 }
